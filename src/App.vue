53d33fe0d127dc19bd62e6a393ad493dfcfb1974
CesarTracana <cedtg12@gmail.com>
2024-04-27 11:41:06 -0400

    Pokemon guesser

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+  import getPokemonOptions from './api/getPokemonOptions';
+
+  
+  getPokemonOptions()
+</script>
+
+<template>
+<p class="text-9xl">hola</p>
+</template>
+
